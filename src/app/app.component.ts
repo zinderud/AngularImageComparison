5a00c067f7a45684fec851b9d2a883ac63a6abd5
zinderud <mokosam@gmail.com>
2018-10-25 04:07:39 +0300

    add ImageCompareComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
-  constructor() { }
+  imageData: string[];
+  constructor() {
+    this.imageData = ['https://preview.ibb.co/jrsA6R/img12.jpg', 'https://preview.ibb.co/mwsA6R/img7.jpg'];
+   }
 
   ngOnInit() {
 
--- src/app/image-compare/image-compare/image-compare.component.ts
+++ src/app/image-compare/image-compare/image-compare.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit , ViewChild, ViewChildren, ElementRef, QueryList, Renderer2} from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ViewChildren, ElementRef, QueryList, Renderer2, Input } from '@angular/core';
 import * as ImageComparison from 'image-comparison';
 import 'style-loader!image-comparison/src/ImageComparison.css';
 @Component({
@@ -7,33 +7,36 @@ import 'style-loader!image-comparison/src/ImageComparison.css';
   styleUrls: ['./image-compare.component.css']
 })
 export class ImageCompareComponent implements OnInit, AfterViewInit {
+
+  @Input( ) imageSrcs: string[];
   @ViewChild('comparisonContainer') comparisonContainer: ElementRef;
 
- @ViewChildren('comparisonImage') comparisonImages: QueryList<any>;
- constructor(private renderer: Renderer2, ) { }
+  @ViewChildren('comparisonImage') comparisonImages: QueryList<any>;
+  constructor(private renderer: Renderer2, ) { }
 
- ngOnInit() {
+  ngOnInit() {
 
- }
 
-ngAfterViewInit() {
-     this.createImageComparison();
+  }
 
-}
+  ngAfterViewInit() {
+    this.createImageComparison();
 
- private createImageComparison(): any {
-   const comparison = new ImageComparison({
-     container: this.comparisonContainer.nativeElement,
-     startPosition: 70,
-     data: this.comparisonImages.map((element: any) => {
-       return {
-         image: element.nativeElement,
-       };
-     }),
-   });
+  }
 
-   comparison._setImageSize();
- }
+  private createImageComparison(): any {
+    const comparison = new ImageComparison({
+      container: this.comparisonContainer.nativeElement,
+      startPosition: 70,
+      data: this.comparisonImages.map((element: any) => {
+        return {
+          image: element.nativeElement,
+        };
+      }),
+    });
+
+    comparison._setImageSize();
+  }
 
 }
 
